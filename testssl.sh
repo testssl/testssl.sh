2ba4e8147371934f221f56e8e113cc9cbbd503f1
Dirk Wetter <dirk@testssl.sh>
2026-07-12 19:31:18 +0200

    Prepare for snapshot release

--- testssl.sh
+++ testssl.sh
@@ -122,7 +122,7 @@ trap "child_error" USR1
 
 ########### Internal definitions
 #
-declare -r VERSION="3.3dev"
+declare -r VERSION="3.3dev-snapshot-2607"
 # shellcheck disable=SC2034
 declare -r SWCONTACT="dirk aet testssl dot sh"
 [[ "$VERSION" =~ dev|rc|beta|snapshot ]] && \
